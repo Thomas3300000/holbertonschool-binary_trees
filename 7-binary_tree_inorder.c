94ec27b1ae207a1a124fa25c58dd67df023bf8f9
Thomas3300000 <8844@holbertonstudents.com>
2024-04-08 16:29:35 +0000

    Fix condition if

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,10 +11,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		binary_tree_inorder(tree->left, func);
 		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
-
+}
